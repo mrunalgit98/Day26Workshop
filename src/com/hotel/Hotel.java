82f257003cc911e94e49aff226f0e3d102ccb3b7
mrunal <mrunalbhoir100@gmail.com>
2022-08-10 11:48:10 +0530

    ablitity to find chepest hotel

--- src/com/hotel/Hotel.java
+++ src/com/hotel/Hotel.java
@@ -1,27 +1,38 @@
 package com.hotel;
 
 public class Hotel {
-    private String name;
-    private int price;
-    public Hotel(String name, int price) {
-        this.name = name;
-        this.price = price;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getPrice() {
-        return price;
-    }
-    public void setPrice(int price) {
-        this.price = price;
-    }
-    @Override
-    public String toString() {
-        return "\nHotelDetails [name=" + name + ", price=" + price + "]";
-    }
 
-}
+        String hotelName;
+        int rating; int weekdayRate; int weekendRate;int specialWeekdayRate;int specialWeekendRate;int avgRate = weekdayRate + weekendRate;
+
+    public Hotel(String hotelName, int rating, int weekdayRate, int weekendRate, int specialWeekdayRate, int specialWeekendRate) {
+        this.hotelName = hotelName; this.rating = rating; this.weekdayRate = weekdayRate;  this.weekendRate = weekendRate;this.specialWeekdayRate = specialWeekdayRate;
+        this.specialWeekendRate = specialWeekendRate;
+    }
+    public int getAvgRate() {
+        return avgRate;
+    }
+    public void setAvgRate(int avgRate) {
+        this.avgRate = avgRate;
+    }
+    public String getHotelName() {
+        return hotelName;
+    }
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {this.rating = rating;}
+    public int getWeekdayRate() {return weekdayRate;}
+    public void setWeekdayRate(int weekdayRate) {this.weekdayRate = weekdayRate;}
+    public int getWeekendRate() {return weekendRate;}
+    public void setWeekendRate(int weekendRate) {this.weekendRate = weekendRate;}
+    public int getSpecialWeekdayRate() {return specialWeekdayRate;}
+    public void setSpecialWeekdayRate(int specialWeekdayRate) {this.specialWeekdayRate = specialWeekdayRate;}
+    public int getSpecialWeekendRate() {return specialWeekendRate;}
+    public void setSpecialWeekendRate(int specialWeekendRate) {this.specialWeekendRate = specialWeekendRate;}
+
+
+    }
